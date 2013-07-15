56e5085332a537070685f9dcff0cd98f7e118dc4
Mike Rogers <mike.r@fullondesign.co.uk>
2013-07-16 00:18:22 +0100

    inital import

---
+++ README.md
@@ -0,0 +1,3 @@
+## MikeRogers.io ##
+
+My personal website which shows off bits of my portfolio and ramblings from my blog.
---
+++ _plugins/image_tag.rb
@@ -0,0 +1,32 @@
+# Based upon https://github.com/mrdanadams/jekyll-thumbnailer/blob/master/thumbnail.rb
+# Usage
+# {% img /uploads/2009/06/apple_cant_do_math.jpg 700x478 "In the background ..." %}
+
+module Jekyll
+  class ImgTag < Liquid::Tag
+
+    def initialize(tag_name, markup, tokens)
+      if /(?<src>[^\s]+)\s+(?<dimensions>[^\s]+)\s+[?\"](?<alt>[^\"]+)/i =~ markup
+        @src = src
+        @alt = alt
+
+        /(?<width>\d+)?x(?<height>\d+)?/ =~ dimensions
+
+        @width = width
+        @height = height
+      end
+      super
+    end
+
+    def render(context)
+
+      html = '<a href="'+@src+'" class="resImg" style="max-width: '+@width+'px; max-height: '+@height+'px;">'
+      html << '<img src="'+@src+'" alt="'+@alt+'"/>'
+      html << '</a>'
+      return html;
+
+    end
+  end
+end
+
+Liquid::Template.register_tag('img', Jekyll::ImgTag)
