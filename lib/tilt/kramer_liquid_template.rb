e13759a5adcf1bfaf6ae9008a4a94f81e6f70ed2
Mike Rogers <me@mikerogers.io>
2017-11-26 15:06:46 +0000

    Making layout a bit more lovely

--- lib/tilt/kramer_liquid_template.rb
+++ lib/tilt/kramer_liquid_template.rb
@@ -4,6 +4,10 @@ require 'middleman-core/renderers/kramdown'
 class Tilt::KramerLiquidTemplate < Middleman::Renderers::KramdownTemplate
   def evaluate(context, *)
     output = super
+
+    output.gsub!('<p>{', '{')
+    output.gsub!('}</p>', '}')
+
     template = Liquid::Template.parse(output)
     template.render() 
   end
