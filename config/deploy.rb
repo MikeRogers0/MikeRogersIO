d08c0885a7153380e16d1386c1643156ae6f914a
Mike Rogers <me@mikerogers.io>
2018-07-28 15:43:17 +0100

    Ditching generated riffraff

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,13 +15,6 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, -> { "/home/ubuntu/var/www/#{fetch(:application)}" }
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
 # Default value for :pty is false
 set :pty, true
 
@@ -31,21 +24,6 @@ append :linked_files, 'config/puma.rb', '.env'
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 append :linked_dirs, '.bundle', 'node_modules', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'build'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-
-# Use our deploy user for the tmp file
-#set :tmp_dir, '/home/ubuntu/tmp'
 
 # Yarn - Make sure it works with our version of node
 set :yarn_flags, '--production --silent --no-progress --ignore-engines'
