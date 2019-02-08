a18ffcf561fb498d49d6ea7c41ba2a70514ace03
Mike Rogers <me@mikerogers.io>
2019-02-08 21:33:57 +0000

    Adjusting amount of languages I highlight in my code

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -6,9 +6,17 @@ var codeBlocks = document.querySelectorAll('pre code');
 codeBlocks.forEach(function(codeBlock){
   if( codeBlock.innerText.includes('<!--') ){
     codeBlock.setAttribute('data-language', 'html')
+  } else if ( codeBlock.innerText.includes('<?xml') ) {
+    codeBlock.setAttribute('data-language', 'xml')
+  } else if ( codeBlock.innerText.includes('#!/') ) {
+    codeBlock.setAttribute('data-language', 'shell')
+  } else if ( codeBlock.innerText.includes('$') ) {
+    codeBlock.setAttribute('data-language', 'css')
+  } else if ( codeBlock.innerText.includes('.rb') || codeBlock.innerText.includes(' do')  || codeBlock.innerText.includes('# ') ) {
+    codeBlock.setAttribute('data-language', 'ruby')
   } else if ( codeBlock.innerText.includes('$(') ) {
     codeBlock.setAttribute('data-language', 'javascript')
   } else {
-    codeBlock.setAttribute('data-language', 'ruby')
+    codeBlock.setAttribute('data-language', 'text')
   }
 });
