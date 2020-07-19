aa22383f54e42bbf6beaa56dcdefc71e44851de4
Mike Rogers <me@mikerogers.io>
2020-07-19 13:41:32 +0100

    Conditionally adding the file hash into assets

--- webpack.config.js
+++ webpack.config.js
@@ -14,16 +14,14 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "build", "_bridgetown", "static", "js"),
-    // filename: "all.[contenthash].js",
-    filename: "all.js",
+    filename: (process.env.NODE_ENV === 'production' ? "all.[contenthash].js" : "all.js" ),
   },
   resolve: {
     extensions: [".js", ".jsx"],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      // filename: "../css/all.[contenthash].css",
-      filename: "../css/all.css",
+      filename: (process.env.NODE_ENV === 'production' ? "../css/all.[contenthash].css" : "../css/all.css" ),
     }),
     new ManifestPlugin({
       fileName: path.resolve(__dirname, ".bridgetown-webpack", "manifest.json"),
