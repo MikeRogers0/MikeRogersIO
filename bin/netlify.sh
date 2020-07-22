70cde6a187af8975448fad50072f07628b0f90a6
Mike Rogers <me@mikerogers.io>
2020-07-23 00:16:29 +0100

    No need for site url

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -7,6 +7,5 @@ CSS_PATH=`find build/_bridgetown/static/css/*.css -type f | sed -e 's,build\/,/,
 
 echo "CSS Path: ${CSS_PATH}"
 
-sed -i s,SITE_URL,${URL},g netlify.toml
 sed -i s,CSS_PATH,${CSS_PATH},g netlify.toml
 sed -i s,NETLIFY_IMAGES_CDN_DOMAIN,${NETLIFY_IMAGES_CDN_DOMAIN},g netlify.toml
