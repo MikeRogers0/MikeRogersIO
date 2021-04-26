9df6733d1d3bd9078d8caa53c1a918055736e678
Mike Rogers <me@mikerogers.io>
2021-04-26 12:57:06 +0100

    Moving back to default output folder

--- postcss.config.js
+++ postcss.config.js
@@ -10,21 +10,13 @@ let environment = {
     // https://github.com/postcss/postcss-custom-properties
     // Add fallbacks when we use CSS variables
     require('postcss-custom-properties'),
-    
   ]
 }
 
 // Only add these in production as they slow down the dev build time a bunch.
 if (process.env.BRIDGETOWN_ENV === "production") {
   environment.plugins.push(
-    // https://github.com/TrySound/postcss-inline-svg
-    // Inline SVGs:
-    // background: svg-load('img/arrow-up.svg', fill=#000, stroke=#fff);
-    // require('postcss-inline-svg'),
-    //require('postcss-svgo')
-
     require('cssnano')({ preset: 'default' }),
-    
   )
 };
 
